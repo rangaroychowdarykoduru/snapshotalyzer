9a810809c384b4c526ec58d7e0f3b20e883e0ea8
Ranga Roy Koduru <ranga@tagontech.com>
2018-09-25 18:44:20 -0400

    added start and stop commands

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,9 +4,26 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
-    for i in ec2.instances.all():
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{"Name":"tag:Project", "Values":[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Command to operate on instances"""
+
+@instances.command("list")
+@click.option('--project', default=None, help="Only instances for project (tag Project: <Name>)")
+def list_instances(project):
+
+    for i in filter_instances(project):
         print(', '.join((
             i.id,
             i.instance_type,
@@ -16,5 +33,21 @@ def list_instances():
             i.public_ip_address
         )))
 
+@instances.command("stop")
+@click.option('--project', default=None, help="Stop only instances that belong to specific project")
+def stop_instances(project):
+
+    for i in filter_instances(project):
+        print('Stopping {0} '.format(i.id))
+        i.stop()
+
+@instances.command("start")
+@click.option('--project', default=None, help="Stop only instances that belong to specific project")
+def stop_instances(project):
+
+    for i in filter_instances(project):
+        print('Starting {0} '.format(i.id))
+        i.start()
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
